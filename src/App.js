f14b94053f764b9a91f0161078e54b9e8b427fe3
CJ <chiji.davids@yahoo.com>
2021-02-12 07:08:41 -0700

    added routes and updated component links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/projects/ProjectDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import CreateProject from "./components/projects/CreateProject";
 
 function App() {
   return (
@@ -10,8 +13,11 @@ function App() {
       <div className="App">
         <Navbar />
         <Switch>
-          <Route exact path="/project-plan/" component={Dashboard} />
-          <Route path="/project-plan/project/:id" component={ProjectDetails} />
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/create" component={CreateProject} />
         </Switch>
       </div>
     </BrowserRouter>
