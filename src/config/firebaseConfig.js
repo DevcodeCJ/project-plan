6f6f27c84bf73cb325c088fe039c7c0899192fa2
CJ <chiji.davids@yahoo.com>
2021-03-05 05:35:58 -0700

    hiding config data

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,16 +2,26 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
+// CONFIG DATA
+const apiKey = process.env.REACT_APP_FIREBASECONFIG_APIKEY;
+const authDomain = process.env.REACT_APP_FIREBASECONFIG_AUTHDOMAIN;
+const projectId = process.env.REACT_APP_FIREBASECONFIG_PROJECTID;
+const storageBucket = process.env.REACT_APP_FIREBASECONFIG_STORAGEBUCKET;
+const messagingSenderId =
+  process.env.REACT_APP_FIREBASECONFIG_MESSAGINGSENDERID;
+const appId = process.env.REACT_APP_FIREBASECONFIG_APPID;
+const measurementId = process.env.REACT_APP_FIREBASECONFIG_MEASUREMENTID;
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  apiKey: "AIzaSyBSUa_gryppe1c0UB17wumhZvhrm3pH4IQ",
-  authDomain: "project-app-db898.firebaseapp.com",
-  projectId: "project-app-db898",
-  storageBucket: "project-app-db898.appspot.com",
-  messagingSenderId: "150711403124",
-  appId: "1:150711403124:web:0a756a72dd4baed91dc721",
-  measurementId: "G-ZRBYTN898R",
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
