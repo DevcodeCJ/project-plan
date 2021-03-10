6b9391393c3ac182f649342b38a7938e6ffda7c3
CJ <chiji.davids@yahoo.com>
2021-03-10 08:09:12 -0700

    secured keys

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,7 +3,7 @@ import "firebase/firestore";
 import "firebase/auth";
 
 // CONFIG DATA
-// const apiKey = process.env.REACT_APP_FIREBASECONFIG_APIKEY;
+const apiKey = process.env.REACT_APP_FIREBASECONFIG_APIKEY;
 const authDomain = process.env.REACT_APP_FIREBASECONFIG_AUTHDOMAIN;
 const projectId = process.env.REACT_APP_FIREBASECONFIG_PROJECTID;
 const storageBucket = process.env.REACT_APP_FIREBASECONFIG_STORAGEBUCKET;
@@ -15,7 +15,7 @@ const measurementId = process.env.REACT_APP_FIREBASECONFIG_MEASUREMENTID;
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  apiKey=API_KEY,
+  apiKey,
   authDomain,
   projectId,
   storageBucket,
